e7b12d7e70a21ba9de11fbb34ab462b139c1f72e
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2024-11-15 18:45:10 +0500

    added structure for Prototype package

---
+++ prototipe/prototipe.go
@@ -0,0 +1,21 @@
+package prototipe
+
+type Prototype struct {
+	value interface{}
+}
+
+func NewPrototype(v interface{}) *Prototype {
+	return &Prototype{value: v}
+}
+
+func NewPrototypeCopy(v interface{}) Prototype {
+	return Prototype{value: v}
+}
+
+func (p Prototype) Value() interface{} {
+	return p.value
+}
+
+func (p *Prototype) SetValue(v interface{}) {
+	p.value = v
+}
