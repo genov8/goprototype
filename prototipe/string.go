1dc47ee2ec049c2a395373d21cff5266ffa9eb54
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2024-11-19 18:27:39 +0500

    adds method to upper for string

--- prototipe/string.go
+++ prototipe/string.go
@@ -1,10 +1,20 @@
 package prototipe
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 func (p *Prototype) Concat(s string) (*Prototype, error) {
 	if str, ok := p.value.(string); ok {
 		return &Prototype{value: str + s}, nil
+	}
+	return nil, errors.New("value is not a string")
+}
+
+func (p *Prototype) ToUpper() (*Prototype, error) {
+	if str, ok := p.value.(string); ok {
+		return &Prototype{value: strings.ToUpper(str)}, nil
 	}
 	return nil, errors.New("value is not a string")
 }
