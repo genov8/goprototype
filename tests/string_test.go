f10e96e6b28f046abc8ac4535e10ec2f98f4b321
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2024-11-20 18:18:35 +0500

    adds tests for upperfirst and lower

--- tests/string_test.go
+++ tests/string_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"fmt"
 	"goprototipe/prototipe"
 	"testing"
 )
@@ -14,5 +15,30 @@ func TestToUpper(t *testing.T) {
 
 	if upperStr.Value() != "HELLO" {
 		t.Errorf("Expected 'HELLO', got %v", upperStr.Value())
+	}
+}
+
+func TestToLower(t *testing.T) {
+	str := prototipe.NewPrototype("HELLO")
+	upperStr, err := str.ToLower()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if upperStr.Value() != "hello" {
+		t.Errorf("Expected 'hello', got %v", upperStr.Value())
+	}
+}
+
+func TestToUpperFirst(t *testing.T) {
+	str := prototipe.NewPrototype("HELLO")
+	upperStr, err := str.ToUpperFirst()
+	fmt.Println(upperStr)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if upperStr.Value() != "Hello" {
+		t.Errorf("Expected 'Hello', got %v", upperStr.Value())
 	}
 }
