6fcaad5311c4e54185f2a14acf9fb703770af2e0
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-02-02 14:06:56 +0500

    moved IsEmpty func

--- prototype/common.go
+++ prototype/common.go
@@ -7,18 +7,4 @@ import (
 
 
 
-func (p *Prototype) IsEmpty() (*Prototype, error) {
-	switch v := p.value.(type) {
-	case string:
-		return &Prototype{value: v == ""}, nil
-	case []interface{}:
-		return &Prototype{value: len(v) == 0}, nil
-	case int:
-		return &Prototype{value: v == 0}, nil
-	case float64:
-		return &Prototype{value: v == 0}, nil
-	default:
-		return nil, errors.New("unsupported type for IsEmpty")
-	}
-}
 
--- prototype/prototype.go
+++ prototype/prototype.go
@@ -119,6 +119,21 @@ func (p *Prototype) Type() *Prototype {
 	return &Prototype{value: typeName}
 }
 
+func (p *Prototype) IsEmpty() (*Prototype, error) {
+	switch v := p.value.(type) {
+	case string:
+		return &Prototype{value: v == ""}, nil
+	case []interface{}:
+		return &Prototype{value: len(v) == 0}, nil
+	case int:
+		return &Prototype{value: v == 0}, nil
+	case float64:
+		return &Prototype{value: v == 0}, nil
+	default:
+		return nil, errors.New("unsupported type for IsEmpty")
+	}
+}
+
 func reverseString(input string) string {
 	runes := []rune(input)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
