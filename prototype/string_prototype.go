82c587e7b2b083cc2ff8db645cb957a50a904325
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-02-07 18:20:11 +0500

    change input and check type

--- prototype/string_prototype.go
+++ prototype/string_prototype.go
@@ -1,11 +1,22 @@
 package prototype
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type StringPrototype struct {
 	*Prototype
 }
 
-func NewStringPrototype(v string) *StringPrototype {
-	return &StringPrototype{
-		Prototype: &Prototype{value: v},
+func NewStringPrototype(v interface{}) *StringPrototype {
+	if reflect.TypeOf(v).Kind() != reflect.String {
+		return &StringPrototype{
+			Prototype: &Prototype{
+				value: nil,
+				err:   fmt.Errorf("invalid type for StringPrototype: must be a slice"),
+			},
+		}
 	}
+	return &StringPrototype{Prototype: &Prototype{value: v}}
 }
