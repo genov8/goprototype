23a896a603339f6ae8163d0a437edd296928d5b7
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-02-10 23:15:48 +0500

    remove chain feature

--- prototype/prototype.go
+++ prototype/prototype.go
@@ -23,30 +23,6 @@ func (p *Prototype) SetValue(v interface{}) {
 	p.value = v
 }
 
-type Chain struct {
-	Prototype *Prototype
-	Err       error
-}
-
-func NewChain(p *Prototype) *Chain {
-	return &Chain{Prototype: p}
-}
-
-func (c *Chain) Must() *Prototype {
-	if c.Err != nil {
-		panic(c.Err)
-	}
-	return c.Prototype
-}
-
-func (c *Chain) Invoke(method func(*Prototype) (*Prototype, error)) *Chain {
-	if c.Err != nil {
-		return c
-	}
-	c.Prototype, c.Err = method(c.Prototype)
-	return c
-}
-
 func (p *Prototype) Error() error {
 	return p.err
 }
