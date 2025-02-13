39ff033e4f60901712138bb4d68412cbbdff55c1
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2025-02-13 13:36:46 +0700

    adds get type method

--- prototype/number_prototype.go
+++ prototype/number_prototype.go
@@ -1,6 +1,9 @@
 package prototype
 
-import "errors"
+import (
+	"errors"
+	"reflect"
+)
 
 type NumberPrototype struct {
 	*Prototype
@@ -14,5 +17,19 @@ func NewNumberPrototype(v interface{}) *NumberPrototype {
 		return &NumberPrototype{Prototype: &Prototype{value: v}}
 	default:
 		return &NumberPrototype{Prototype: &Prototype{value: 0, err: errors.New("invalid type for NumberPrototype")}}
+	}
+}
+
+func (p *Prototype) getNumberType() interface{} {
+	switch v := p.value.(type) {
+	case int, int8, int16, int32, int64:
+		return reflect.ValueOf(v).Int()
+	case uint, uint8, uint16, uint32, uint64:
+		return int64(reflect.ValueOf(v).Uint())
+	case float32, float64:
+		return reflect.ValueOf(v).Float()
+	default:
+		p.err = errors.New("value is not a number")
+		return nil
 	}
 }
